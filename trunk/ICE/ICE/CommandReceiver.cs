ebd55007b0d3caeea1611ed5ad7a125f4a4d42c1
Casper Thule <c.thulehansen@live.com>
2013-12-10 13:46:49 +0100

    Created the solution. Created the ICE project. Created the initial commandreceiver an directory command receiver.

---
+++ trunk/ICE/ICE/CommandReceiver.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE
+{
+    public class CommandReceiver
+    {
+        public void NewCommand(string[] args)
+        {
+        }
+    }
+}
